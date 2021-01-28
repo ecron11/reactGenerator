66bcd95597ebcb5321b5ff48987956fe764f6738
Erik Longuepee <erik.longuepee@gmail.com>
2021-01-29 00:42:00 +0100

    added formatting for react functional components

--- reactGenerator.py
+++ reactGenerator.py
@@ -7,15 +7,31 @@ templateFile.close()
 
 outputFile = open("./reactFiles/" + template["templateName"] +".js", "w")
 
+indentCharacter = "\t"
+currentIndent = 0
+
     ## React Functional component generator
 if template["templateType"] == "reactFunctional":
-    outputFile.write("import React from 'react' \n\n")
-    outputFile.write("export default function " + template["templateName"] + "(props) { \nreturn (\n")
-    
-    for component in template['template']:
-        outputFile.write("<" + component["component"] + ">\n" + "</" + component["component"] + ">\n")
+    # Need to add imports for any children react elements
 
+    outputFile.write("import React from 'react' \n\n")
+    outputFile.write("export default function " + template["templateName"] + "(props) { \n")
+    
+    currentIndent += 1
+    outputFile.write(currentIndent*indentCharacter + "return (\n")
+    currentIndent += 1
+    for component in template['template']:
+        outputFile.write(currentIndent*indentCharacter + "<" + component["component"] + "\n")
+
+        currentIndent += 1
+        for arg in component["arguments"].keys():
+            outputFile.write(currentIndent*indentCharacter + arg + "='" + component["arguments"][arg] + "'\n")
+        currentIndent -= 1
+        outputFile.write(currentIndent*indentCharacter + ">\n")
+        outputFile.write(currentIndent*indentCharacter + "</" + component["component"] + ">\n")
     #closing parentheses and brackets
-    outputFile.write("\n)\n}")
+    outputFile.write(currentIndent*indentCharacter + "\n)")
+    currentIndent -= 1
+    outputFile.write(currentIndent*indentCharacter + "\n}")
 
 outputFile.close()
